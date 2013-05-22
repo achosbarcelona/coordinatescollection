447049a93b098f4e865cf39f0eae6d5674854ee7
Denis Kabistan <denis@fd9group.com>
2013-05-21 17:48:18 -0700

    Added bottom slider.

---
+++ js/load.js
@@ -0,0 +1,16 @@
+//Custom
+$(document).ready(function(){
+	$('.feature').bxSlider({
+  	slideWidth: 470,
+    minSlides: 4,
+    maxSlides: 4,
+    slideMargin: 1,
+    moveSlides: 1,
+    pager: false,
+    controls: true,
+    prevText: "",
+    nextText: "",
+    auto: false,
+    pause: 8000
+	});
+});
