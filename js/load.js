f870f473b3c26676a2bd051c6a21acd20a2ae83d
Denis Kabistan <denis@fd9group.com>
2013-07-12 12:45:42 -0700

    Animation fixes for site.

--- js/load.js
+++ js/load.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
   $(".logo").delay(1000).animate({"opacity":"1"}, 700);
   $(".made-in-la-logo").delay(1500).animate({"opacity":"1"}, 700);
   $("#learn-more-home").delay(1800).animate({"opacity":"1"}, 700);
-  $("#p-line").delay(1800).animate({"opacity":"1"}, 700);
 
 
   $("#product-url-legend").click(function(){
@@ -23,6 +22,25 @@ $(document).ready(function(){
   $("#product-url-horizon").click(function(){
     $("#overlay-content-horizon").delay(500).css({"display":"block"}).animate({"opacity":"1"}, 700);
   });
+  // Listen for these anchors being called.
+  $("#jump-1").click(function(){
+    $("#mini-globe").delay(1000).animate({"opacity":"1","margin-top":"15px"}, 700);
+    $("#every-memory").delay(1000).animate({"opacity":"1","margin-top":"15px"}, 700);
+    $("#hiw").delay(1200).animate({"opacity":"1","margin-top":"15px"}, 700);
+  });
+
+  $("#jump-2").click(function(){
+    $("#p-line").delay(1000).animate({"opacity":"1","margin-top":"15px"}, 700);
+    $("#p-desc").delay(1200).animate({"opacity":"1","margin-top":"15px"}, 700);
+  });
+
+  $("#jump-3").click(function(){
+    $("#header").delay(1000).animate({"opacity":"1"}, 700);
+    $("#products-load").delay(1300).animate({"opacity":"1"}, 700);
+    $("#no-border").delay(1500).animate({"opacity":"1","margin-top":"15px"}, 700);
+  });
 
 
-});
+
+
+});//Closed
